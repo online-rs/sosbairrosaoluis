b13a8273ae328bd94dd8a301f2197fa9bbf904ed
online-rs <88519041+online-rs@users.noreply.github.com>
2024-05-15 12:54:38 +0000

    facilidades

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,18 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Cadastro from "../components/Cadastro"; // Import with curly braces
-import React from 'react'
+import React from 'react';
+import Facilidades from "@/components/Facilidades";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>SOS Bairro São Luis</h1>
       <Cadastro style={{ backgroundColor: "red" }} />
-    </main>
+      <section>
+        <h1>Facilidades</h1>
+        <Facilidades />
+      </section>
+         </main>
   );
 }
---
+++ src/components/Facilidades.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Facilidades.module.css'
+
+
+function Facilidades(){
+    return(
+        <div className={styles.squad}>
+            <p>Atestado Defesa Civil</p>
+        </div>
+    );
+}
+
+export default Facilidades;
