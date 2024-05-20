11b659822553b0cde697b3e5c5dce1d7c10c3f3d
online-rs <88519041+online-rs@users.noreply.github.com>
2024-05-20 00:17:00 +0000

    atualizaçãosite

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,36 +1,9 @@
-'use client';
-import React, { useState } from 'react';
-import axios from 'axios';
-
 function UserForm() {
-    const [entrada, setEntrada] = useState('');
-
-    const handleInputChange = (e) => {
-        setEntrada(e.target.value);
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        
-        try {
-            const response = await axios.post('http://localhost:3000/api/user', {
-                name: entrada,
-            });
-            
-            console.log('User created:', response.data);
-        } catch (error) {
-            console.error('Error creating user:', error);
-        }
-    };
-
     return (
-        <form className="d-flex flex-column" onSubmit={handleSubmit}>
+        <form className="d-flex flex-column">
             <input
-                name="entrada"
                 className="m-1"
-                value={entrada}
-                onChange={handleInputChange}
-            />
+                />
             <button type="submit" className="m-1 btn btn-primary">Enviar</button>
         </form>
     );
